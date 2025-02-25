d94011bb7df56255612a6a3f24ee0404bfc3803c
cha <yeonhee1021@gmail.com>
2025-02-25 21:20:48 +0900

    a

--- loans/src/main/java/com/eazybytes/loans/LoansApplication.java
+++ loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -8,13 +8,17 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
-@EnableJpaRepositories("com.eazybytes.loans.repository")
-@EntityScan("com.eazybytes.loans.model")*/
+// @ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+// @EnableJpaRepositories("com.eazybytes.loans.repository")
+// @EntityScan("com.eazybytes.loans.model")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @OpenAPIDefinition(
