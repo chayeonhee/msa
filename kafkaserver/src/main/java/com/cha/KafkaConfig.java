ab778c87d700f2145b12981d372733babf8ea430
cha <yeonhee1021@gmail.com>
2025-02-27 19:37:21 +0900

    kafka client

--- kafkaserver/src/main/java/com/cha/KafkaConfig.java
+++ kafkaserver/src/main/java/com/cha/KafkaConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,7 +15,8 @@ import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
-
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapservers;
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         // Kafka 프로듀서 설정
@@ -30,8 +32,8 @@ public class KafkaConfig {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> configProps = new HashMap<>();
-        // configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.221:30494");  // Kafka의 외부 IP와 포트
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.221:30641");  // Kafka의 외부 IP와 포트
+        // configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.221:30641");  // Kafka의 외부 IP와 포트
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapservers);  // Kafka의 외부 IP와 포트
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return configProps;
