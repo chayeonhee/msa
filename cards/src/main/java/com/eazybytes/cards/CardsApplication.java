5ce22cf931194f406dd5648d5b950be941e8a081
cha <yeonhee1021@gmail.com>
2025-03-04 16:41:20 +0900

    aaa

--- cards/src/main/java/com/eazybytes/cards/CardsApplication.java
+++ cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -3,6 +3,7 @@ package com.eazybytes.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -16,6 +17,7 @@ import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 /*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
 @EnableJpaRepositories("com.eazybytes.cards.repository")
 @EntityScan("com.eazybytes.cards.model")*/
